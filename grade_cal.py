a35aef487cbfe811fa98b3c404578843e53cd6f6
Krithika U <krithikaumasankar2007@gmail.com>
2026-04-07 21:55:10 +0530

    Create grade_cal.py

---
+++ grade_cal.py
@@ -0,0 +1,30 @@
+class student():
+    def __init__(self):
+        print("\n\tWELCOME TO GRADE CALCULATOR....!")
+        self.name = input("Enter your name: ")
+        self.marks = []
+    def add_marks(self):
+        for i in range(5):
+            m = float(input("Enter marks: "))
+            self.marks.append(m)
+    def display(self):
+        t = 0
+        for i in self.marks:
+            t += i
+        avg = t / 5
+        if avg >= 90:
+            grade = "A"
+        elif avg >= 80:
+            grade = "B"
+        elif avg >= 70:
+            grade = "C"
+        elif avg >= 60:
+            grade = "D"
+        elif avg >= 50:
+            grade = "R"
+        else:
+            grade = "F"
+        print("Name:",self.name,"\nGrade:",grade,"\ntotal:",t,"\nAverage marks:",avg)
+s = student()
+s.add_marks()
+s.display()
